774ae2e5a1b57279b48c6b427b41a63d0e08e216
Andreas Maggiori <andreas.maggiori@gmail.com>
2019-05-16 13:59:00 +0200

    fixed bug in the number of variables of each edge, which should depend on the arrival time of the particular edge

--- lower_bound_for_preemption.py
+++ lower_bound_for_preemption.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import maximum_matching as mm
 
 
 
@@ -75,8 +75,8 @@ with open("preemption_input.txt", 'r') as f:
 			e = [vL, vR, t]
 			edges_lst.append(e)
 			edge_vector = np.zeros(T*T)
-			edge_vector[t*T] = 1
-			for t_prime in range(t*T+1, (t+1)*T):
+			edge_vector[t*T+t] = 1
+			for t_prime in range(t*T+t+1, (t+1)*T):
 				edge_vector[t_prime] = -1
 			edge_vectors_lst.append(edge_vector)
 			#create or update (depending if the vertex already appeared) his object and update the list
@@ -95,37 +95,83 @@ with open("preemption_input.txt", 'r') as f:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 #here I will construct the degree constraint of each vertex
 
 A_degree_constraints = []
-#constraints for left vertexes
+#degree constraints for left vertexes
 for V in left_vertexes_list:
 	adjacent_edges_arrivals = V.get_adjacent_edges()
 	row_to_add = np.zeros(T*T)
-	print(adjacent_edges_arrivals)
+#	print(adjacent_edges_arrivals)
 	for t in adjacent_edges_arrivals:
 		row_to_add = row_to_add + edge_vectors_lst[t]
 	A_degree_constraints.append(row_to_add)
 
-#constraints for right vertexes
+#degree constraints for right vertexes
 for V in right_vertexes_list:
 	adjacent_edges_arrivals = V.get_adjacent_edges()
 	row_to_add = np.zeros(T*T)
 	for t in adjacent_edges_arrivals:
 		row_to_add = row_to_add + edge_vectors_lst[t]
 	A_degree_constraints.append(row_to_add)
-
-
-
-
-
+#turn list of lists that represent the degree constarints into a numpy matrix
 A_degree_constraints = np.array(A_degree_constraints)
 print(A_degree_constraints)
 
 
 
+exit()
 
 
+
+
+#here I will construct the constraint of the form z_e1 - z_e2 - z_e3 - ...- z_eT >= 0 \forall e \in E
+A_nonneg_edge_constraint = [] 
+for t in range(0,T):
+	A_nonneg_edge_constraint.append(-edge_vectors_lst[t])
+A_nonneg_edge_constraint = np.array(A_nonneg_edge_constraint)
+
+#here I will construct the competitiveness constraints
+A_comp_constraints = []
+accum_lst = [0]*(T*T)
+for t in range(0,T*T):
+	if t%T == 0:	
+		accum_lst[t] = 1
+	else:
+		accum_lst[t] = -1
+	A_comp_constraints.append(accum_lst[:])
+
+A_comp_constraints = np.array(A_comp_constraints)
+
+
+
+for i in range(0,len(edges_lst)):
+	bpGraph  = mm.list_to_adj_matrix(edges_lst[:i+1], num_vtxs)
+	g = mm.GFG(bpGraph) 
+	g.maxBPM()
+
+
+
+
+
+
+
+
+
+
+exit()
 
 for t in range(0,T):
 	print("-"*10)
